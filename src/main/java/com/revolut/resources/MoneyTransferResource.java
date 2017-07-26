f6d7abfdea3a373070887c08741abab3bb67e965
Saeid <saeidrzm@gmail.com>
2017-07-26 19:38:27 +0200

    FIX#Fix validation

--- src/main/java/com/revolut/resources/MoneyTransferResource.java
+++ src/main/java/com/revolut/resources/MoneyTransferResource.java
@@ -40,18 +40,22 @@ public class MoneyTransferResource {
     }
 
     private boolean validateParams(TransferRequest request) {
-        boolean invalid;
-        if (invalid = request.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
+        boolean invalid = false;
+        if (!invalid && request.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
             LOG.warn("VALIDATION_ERROR: Can't transfer amount {} less or equal to zero", request.getAmount());
+            invalid = true;
         }
-        if (invalid = !invalid && request.getFromAcc() == request.getToAcc()) {
-            LOG.warn("VALIDATION_ERROR: Can't send money to the same account {} = {}", request.getFromAcc(), request.getToAcc());
-        }
-        if (invalid = !invalid && service.getBalance(request.getFromAcc()) == null) {
+        if (!invalid && service.getBalance(request.getFromAcc()) == null) {
             LOG.warn("VALIDATION_ERROR: Can't find sender account {}", request.getFromAcc());
+            invalid = true;
         }
-        if (invalid = !invalid && service.getBalance(request.getToAcc()) == null) {
+        if (!invalid && service.getBalance(request.getToAcc()) == null) {
             LOG.warn("VALIDATION_ERROR: Can't find recipient account {}", request.getToAcc());
+            invalid = true;
+        }
+        if (!invalid && request.getFromAcc() == request.getToAcc()) {
+            LOG.warn("VALIDATION_ERROR: Can't send money to the same account. From {} to {}", request.getFromAcc(), request.getToAcc());
+            invalid = true;
         }
         return !invalid;
     }
